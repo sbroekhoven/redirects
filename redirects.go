16cd52b6d4e7ae760f830170ebf5f293f0754704
Sebastian Broekhoven <2205121+sbro101@users.noreply.github.com>
2024-12-03 22:58:22 +0100

    Prepare for release v1.0.7

--- redirects.go
+++ redirects.go
@@ -2,9 +2,11 @@ package redirects
 
 import (
 	"crypto/tls"
+	"errors"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
 
 // Data struct
@@ -24,29 +26,46 @@ type Redirects struct {
 	TLSVersion string `json:"tlsversion,omitempty"` // Dont know if TLS version stays.
 }
 
+const maxRedirects = 20
+
 // Get function
+// This function takes a URL and a nameserver as arguments and returns a struct
+// with information about the URL and the redirects it goes through.
+// The function will follow a maximum of 20 redirects.
+// If there is an error when making the request, the function will return an error
+// message.
+// If a redirect goes to an invalid URL, the function will not return an error,
+// but instead will set the Error field of the returned struct to true and the
+// ErrorMessage field to a string with the error message.
+// The TLS version is currently not really relevant, but it is included in the
+// returned struct.
 func Get(redirecturl string, nameserver string) *Data {
+
 	r := new(Data)
 
 	r.URL = redirecturl
-	_, err := url.Parse(redirecturl)
+
+	err := validateURL(redirecturl)
 	if err != nil {
 		r.Error = true
 		r.ErrorMessage = err.Error()
 		return r
 	}
 
-	var i int
+	// Create a new HTTP client
+	client := createHTTPClient()
 
-	// we want to follow max 20 redirects
-	for i < 20 {
-		// set client to CheckRedirect, not following the redirect
-		client := &http.Client{
-			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return http.ErrUseLastResponse
-			}}
+	// Loop through up to 20 redirects
+	for i := 0; i < maxRedirects; i++ {
 
-		// redirecturl prefix check for incomplete
+		// Set the client to follow redirects, but not to follow the redirect
+		// automatically. Instead, the redirect will be stored in the Location
+		// header and the client will stop following redirects.
+		client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		}
+
+		// Add http:// to url if missing
 		if !caseInsenstiveContains(redirecturl, "http://") && !caseInsenstiveContains(redirecturl, "https://") {
 			// TODO: Set warning
 			redirecturl = "http://" + redirecturl
@@ -55,50 +74,105 @@ func Get(redirecturl string, nameserver string) *Data {
 		// Prepare the request
 		req, err := http.NewRequest("GET", redirecturl, nil)
 		if err != nil {
+			// If there is an error with the request, set the Error field to true
+			// and the ErrorMessage field to the error message.
 			r.Error = true
 			r.ErrorMessage = err.Error()
 			return r
 		}
 
-		// Set User-Agent
-		req.Header.Set("User-Agent", "Golang_Research_Bot/3.0")
+		// Set the User-Agent header
+		req.Header.Set("User-Agent", "Mozilla/5.0 (Golang_Research_Bot/3.0)")
 
-		// Do the request.
+		// Do the request
 		resp, err := client.Do(req)
 		if err != nil {
+			// If there is an error with the request, set the Error field to true
+			// and the ErrorMessage field to the error message.
 			r.Error = true
 			r.ErrorMessage = err.Error()
 			return r
 		}
 		defer resp.Body.Close()
 
-		// Set soms vars.
+		// Create a new Redirects struct
 		redirect := new(Redirects)
+		if redirect == nil {
+			// If the Redirects struct is nil, set the Error field to true and the
+			// ErrorMessage field to the error message.
+			r.Error = true
+			r.ErrorMessage = "redirect == nil"
+			return r
+		}
+
+		// Set the fields of the Redirects struct
 		redirect.Number = i
 		redirect.StatusCode = resp.StatusCode
 		redirect.URL = resp.Request.URL.String()
 		redirect.Protocol = resp.Proto
 
-		// Dont know if TLS version stays. Not really relevant.
+		// If the URL is an https URL, get the TLS version
 		if caseInsenstiveContains(redirecturl, "https://") {
 			redirect.TLSVersion = tls.VersionName(resp.TLS.Version)
 		} else {
 			redirect.TLSVersion = "N/A"
 		}
 
+		// Add the Redirects struct to the slice of Redirects structs
 		r.Redirects = append(r.Redirects, redirect)
 
+		// If the status code is 200 or greater than 303, break out of the loop
 		if resp.StatusCode == 200 || resp.StatusCode > 303 {
 			break
 		} else {
+			// Get the URL from the Location header
 			redirecturl = resp.Header.Get("Location")
+			if redirecturl == "" {
+				// If the Location header is empty, set the Error field to true and
+				// the ErrorMessage field to the error message.
+				r.Error = true
+				r.ErrorMessage = "Location header is empty"
+				return r
+			}
+
+			// Ensure redirecturl is fully qualified
+			if !strings.HasPrefix(redirecturl, "http://") && !strings.HasPrefix(redirecturl, "https://") {
+				redirecturl = "http://" + redirecturl
+			}
+
+			// Increment the loop counter
 			i++
 		}
 	}
 
+	// Return the Data struct
 	return r
 }
 
 func caseInsenstiveContains(a, b string) bool {
 	return strings.Contains(strings.ToUpper(a), strings.ToUpper(b))
+}
+
+func createHTTPClient() *http.Client {
+	return &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Timeout: 30 * time.Second,
+	}
+}
+
+func validateURL(redirecturl string) error {
+
+	if redirecturl == "" {
+		return errors.New("empty URL")
+	}
+	// Parse the URL using the url.Parse() function
+	_, err := url.Parse(redirecturl)
+	if err != nil {
+		// If the URL is invalid, return the error
+		return err
+	}
+	// If the URL is valid, return nil
+	return nil
 }
